68759b41e5336d418874dfaf6eae1735cc5f943b
Siddhartha Gopala <siddhu.gopala@gmail.com>
2026-04-03 00:05:40 +0530

    chore: update package.json and dependencies, rename project, and add nativewind preset
    - Renamed project from "PPulseCheck" to "ppulse-check"
    - Removed deprecated dependency "@react-native/new-app-screen"
    - Added new dependency "react-native-worklets"
    - Updated "@react-native-community/cli" and related packages to version "^15.0.1"
    - Updated tailwind.config.js to include nativewind preset
    - Updated tsconfig.json to include "nativewind-env.d.ts" in the include array

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
-    ['module:react-native-dotenv', {
-      "moduleName": "@env",
-      "path": ".env",
-    }],
-    'nativewind/babel',
-    'react-native-reanimated/plugin'
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+    'react-native-reanimated/plugin',
   ],
 };
